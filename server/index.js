6e6ce18542c5916eaa9f21d10a5ab627166f1427
Ivan <i85chiosa@gmail.com>
2022-11-30 20:55:47 +0100

    Mongo Db

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 // configuration 
 
@@ -61,4 +64,8 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
 }).then(() =>{
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    // User.insertMany(users);
+    // Post.insertMany(posts); 
+
 }).catch((error) => console.log(`${error} did not connect`));
